1a2123ca24aac190ccfdde99c761cc35e14e8a66
Brian Langenberger <bjl@usa.net>
2023-03-24 11:01:32 -0500

    Add summerize trait as a related option

--- src/lib.rs
+++ src/lib.rs
@@ -66,6 +66,65 @@
 //! assert!(v.iter().cloned().group::<Result<HashMap<_, Vec<_>>, ()>>().is_err());
 //! assert!(v.iter().cloned().group::<Result<BTreeMap<_, Vec<_>>, ()>>().is_err());
 //! ```
+//!
+//! Also offers the option to extend iterators for summarizing their values
+//! into a mapping type whose values are a sum.
+//!
+//! # Example 1
+//! ```
+//! use std::collections::{BTreeMap, HashMap};
+//! use iter_group::IntoSummary;
+//!
+//! let v = vec![('a', 1), ('b', 1), ('a', 2), ('b', 2), ('a', 3)];
+//!
+//! let h = v.iter().cloned().summarize::<HashMap<_, _>>();
+//! assert_eq!(h.get(&'a'), Some(&6));
+//! assert_eq!(h.get(&'b'), Some(&3));
+//!
+//! let b = v.iter().cloned().summarize::<BTreeMap<_, _>>();
+//! assert_eq!(b.get(&'a'), Some(&6));
+//! assert_eq!(b.get(&'b'), Some(&3));
+//! ```
+//!
+//! ## Example 2
+//! ```
+//! use std::collections::{BTreeMap, HashMap};
+//! use iter_group::IntoSummary;
+//!
+//! let v = vec![Some(('a', 1)), Some(('b', 2)), Some(('a', 3))];
+//!
+//! let h = v.iter().cloned().summarize::<Option<HashMap<_, _>>>().unwrap();
+//! assert_eq!(h.get(&'a'), Some(&4));
+//! assert_eq!(h.get(&'b'), Some(&2));
+//!
+//! let b = v.iter().cloned().summarize::<Option<BTreeMap<_, _>>>().unwrap();
+//! assert_eq!(b.get(&'a'), Some(&4));
+//! assert_eq!(b.get(&'b'), Some(&2));
+//!
+//! let v = vec![Some(('a', 1)), None, Some(('b', 2))];
+//! assert!(v.iter().cloned().summarize::<Option<HashMap<_, _>>>().is_none());
+//! assert!(v.iter().cloned().summarize::<Option<BTreeMap<_, _>>>().is_none());
+//! ```
+//!
+//! ## Example 3
+//! ```
+//! use std::collections::{BTreeMap, HashMap};
+//! use iter_group::IntoSummary;
+//!
+//! let v = vec![Ok(('a', 1)), Ok(('b', 2)), Ok(('a', 3))];
+//!
+//! let h = v.iter().cloned().summarize::<Result<HashMap<_, _>, ()>>().unwrap();
+//! assert_eq!(h.get(&'a'), Some(&4));
+//! assert_eq!(h.get(&'b'), Some(&2));
+//!
+//! let b = v.iter().cloned().summarize::<Result<BTreeMap<_, _>, ()>>().unwrap();
+//! assert_eq!(b.get(&'a'), Some(&4));
+//! assert_eq!(b.get(&'b'), Some(&2));
+//!
+//! let v = vec![Ok(('a', 1)), Err(()), Ok(('b', 2))];
+//! assert!(v.iter().cloned().summarize::<Result<HashMap<_, _>, ()>>().is_err());
+//! assert!(v.iter().cloned().summarize::<Result<BTreeMap<_, _>, ()>>().is_err());
+//! ```
 
 #![warn(missing_docs)]
 #![forbid(unsafe_code)]
@@ -183,5 +242,117 @@ where
     #[inline]
     fn insert_and_extend(&mut self, key: K, value: V) {
         self.entry(key).or_default().extend(std::iter::once(value));
+    }
+}
+
+/// Implemented by mapping types (`HashMap` and `BTreeMap`)
+/// such that they can populated by an iterator.
+/// Roughly analogous to the `FromIterator` trait.
+pub trait SummarizeFromIterator<V> {
+    /// Builds a grouped collection from an iterator of key,value tuples.
+    ///
+    /// It's recommended to call an iterator's `summarize()` method
+    /// rather than call this directly.
+    fn summarize_from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = V>;
+}
+
+impl<K, V, C> SummarizeFromIterator<(K, V)> for C
+where
+    C: InsertAndIncrement<K, V>,
+{
+    #[inline]
+    fn summarize_from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = (K, V)>,
+    {
+        #[derive(Debug)]
+        enum Never {}
+
+        let r: Result<Self, Never> =
+            SummarizeFromIterator::summarize_from_iter(iter.into_iter().map(Ok));
+        r.unwrap()
+    }
+}
+
+impl<K, V, C> SummarizeFromIterator<Option<(K, V)>> for Option<C>
+where
+    C: InsertAndIncrement<K, V>,
+{
+    #[inline]
+    fn summarize_from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = Option<(K, V)>>,
+    {
+        let r: Result<C, ()> =
+            SummarizeFromIterator::summarize_from_iter(iter.into_iter().map(|o| o.ok_or(())));
+        r.ok()
+    }
+}
+
+impl<K, V, C, E> SummarizeFromIterator<Result<(K, V), E>> for Result<C, E>
+where
+    C: InsertAndIncrement<K, V>,
+{
+    fn summarize_from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = Result<(K, V), E>>,
+    {
+        let mut h = C::default();
+
+        for r in iter {
+            let (k, v) = r?;
+            h.insert_and_increment(k, v);
+        }
+
+        Ok(h)
+    }
+}
+
+/// Extends Iterators with an additional grouping method.
+pub trait IntoSummary<V> {
+    /// Consumes the iterator and returns a summarized of its contents.
+    fn summarize<B: SummarizeFromIterator<V>>(self) -> B;
+}
+
+impl<V, I> IntoSummary<V> for I
+where
+    I: Iterator<Item = V>,
+{
+    #[inline]
+    fn summarize<B: SummarizeFromIterator<V>>(self) -> B {
+        B::summarize_from_iter(self)
+    }
+}
+
+/// A trait for mapping types to serve as a trivial wrapper
+/// over the Entry interface
+pub trait InsertAndIncrement<K, V>: Default {
+    /// Creates collection with the given key if necessary
+    /// and increments its total by the given value
+    fn insert_and_increment(&mut self, key: K, value: V);
+}
+
+impl<K, V, H> InsertAndIncrement<K, V> for HashMap<K, V, H>
+where
+    K: Eq + Hash,
+    V: Default + std::ops::AddAssign,
+    H: BuildHasher + Default,
+{
+    #[inline]
+    fn insert_and_increment(&mut self, key: K, value: V) {
+        *self.entry(key).or_default() += value;
+    }
+}
+
+impl<K, V> InsertAndIncrement<K, V> for BTreeMap<K, V>
+where
+    K: Ord,
+    V: Default + std::ops::AddAssign,
+{
+    #[inline]
+    fn insert_and_increment(&mut self, key: K, value: V) {
+        *self.entry(key).or_default() += value;
     }
 }
